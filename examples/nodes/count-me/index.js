18f4bb8b946f85a5d576e8a7b98012f9b8084cc2
Simon Swain <simon@modus.com.au>
2012-11-22 22:24:24 +1100

    Rout-robin pipe example

---
+++ examples/nodes/count-me/index.js
@@ -0,0 +1,17 @@
+var straw = require('../../../lib/straw.js')
+
+/*
+ * emits cumulative count of messages received.
+ */
+
+module.exports = straw.node.extend({
+  title: 'Count',
+  total: 0,
+  process: function(msg, done) {      
+    this.total ++;
+    this.output({key: this.opts.key, count: this.total});    
+    if ( done ) {
+      done(false, {count: this.total});
+    }
+  }
+});
---
+++ examples/ping-balanced.js
@@ -0,0 +1,30 @@
+var straw = require('../lib/straw.js');
+var redis = require('redis');
+var counts = redis.createClient();
+var topo;
+
+topo = new straw.topology({
+  'ping':{
+    'node': __dirname +'/../examples/nodes/ping',
+    'output':'ping-out'
+  },
+  'count-1':{
+    'node': __dirname +'/../examples/nodes/count-me',
+    'input':'ping-out',
+    'output':'count-out'
+  },
+  'count-2':{
+    'node': __dirname +'/../examples/nodes/count-me',
+    'input':'ping-out',
+    'output':'count-out'
+  },
+  'print':{
+    'node': __dirname + '/../examples/nodes/print',
+    'input':['count-out']
+  },
+  'ping-out': {
+    'pipe': 'round-robin',
+    'purge': true
+  },
+
+});
