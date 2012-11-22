34c4c8de68e3f209a54dcbba4742294ef251ed7b
Simon Swain <simon@modus.com.au>
2012-11-22 22:24:49 +1100

    Modernised

--- examples/ping-thru-count.js
+++ examples/ping-thru-count.js
@@ -3,30 +3,27 @@ var redis = require('redis');
 var counts = redis.createClient();
 var topo;
 
-// [ping] -> [passsthru] -> [count]
-
-topo = new straw.topology({
-  'ping':{
-    'node':'../examples/nodes/ping',
-    'output':'ping-out'
-  },
-  'thru':{
-    'node':'../examples/nodes/passthru',
-    'input':'ping-out',
-    'output':'passthru-out'
-  },
-  'count':{
-    'node':'../examples/nodes/count',
-    'input':'passthru-out',
-    'output':'count-out'
-  }
-};
-
 counts.on('message', function(channel, msg){
   msg = JSON.parse(msg);
   console.log(msg);
 }).subscribe('count-out');
 
-var topo = new straw.topology(map, function(err, res){
-  console.log('Topology Created');
+// [ping] -> [thru] -> [count]
+
+topo = new straw.topology({
+  'ping':{
+    'node': __dirname + '/nodes/ping',
+    'output':'ping-out'
+  },
+  'thru':{
+    'node': __dirname + '/nodes/passthru',
+    'input':'ping-out',
+    'output':'passthru-out'
+  },
+  'count':{
+    'node': __dirname + '/nodes/count',
+    'input':'passthru-out',
+    'output':'count-out'
+  }
 });
+
