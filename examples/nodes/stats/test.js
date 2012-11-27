ee50dbf4a0f8fd20a5f32e8db256ab62958c24e8
Simon Swain <simon@modus.com.au>
2012-11-27 16:37:49 +1100

    Added Stats examples.

---
+++ examples/nodes/stats/test.js
@@ -0,0 +1,10 @@
+var Node = require('./index.js');
+
+module.exports = {
+  'create': function(test) {
+    test.expect(1);
+    var node = new Node();
+    test.equal(node.title, 'Stats', 'should be Stats');
+    test.done();
+  }
+};
