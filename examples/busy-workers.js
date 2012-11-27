f027270ae91f3fb6a68f485cfb9ef25d13e59d99
Simon Swain <simon@modus.com.au>
2012-11-27 16:23:40 +1100

    Examples for round-robin nodes and long-running tasks

---
+++ examples/busy-workers.js
@@ -0,0 +1,43 @@
+var straw = require('../lib/straw.js');
+var redis = require('redis');
+var counts = redis.createClient();
+var topo;
+
+topo = new straw.topology({
+  'ping':{
+    'node': __dirname +'/../examples/nodes/ping',
+    'output':'ping-out'
+  },
+  'count':{
+    'node': __dirname +'/../examples/nodes/count',
+    'input':'ping-out',
+    'output':'count-out'
+  },
+  'worker-1':{
+    'node': __dirname +'/../examples/nodes/heavy-lifting',
+    'input':'ping-out',
+    'output':'done-out'
+  },
+  'worker-2':{
+    'node': __dirname +'/../examples/nodes/heavy-lifting',
+    'input':'ping-out',
+    'output':'done-out'
+  },
+  'worker-3':{
+    'node': __dirname +'/../examples/nodes/heavy-lifting',
+    'input':'ping-out',
+    'output':'done-out'
+  },
+  'jobs-sent':{
+    'node': __dirname + '/../examples/nodes/print',
+    'input':['count-out']
+  },
+  'job-done':{
+    'node': __dirname + '/../examples/nodes/print',
+    'input':['done-out']
+  },
+  'ping-out': {
+    'pipe': 'round-robin'
+  },
+
+});
